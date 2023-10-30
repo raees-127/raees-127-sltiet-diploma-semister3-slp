7bf2360860b8ed6a347580b7be6afc2f6c271056
raees-127 <145735246+raees-127@users.noreply.github.com>
2023-10-30 12:17:06 +0530

    Create Python-Program-14.py

---
+++ Python-Program-14.py
@@ -0,0 +1,17 @@
+dict = {
+    '1':'ABC',
+    '2':'BCD',
+    '3':'CDE',
+    '4':'DEF',
+    '5':'EFG'
+}  
+  
+print(dict)
+  
+dict['6'] = 'FGH'
+  
+print(dict)
+
+dict.pop('6')
+
+print(dict)
